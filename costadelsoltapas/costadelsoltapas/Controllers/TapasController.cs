7ce2418da980f1c58079154d23c4487b1794c399
unknown <aroblessilva@outlook.com>
2019-09-24 20:30:51 +0200

    first commit

---
+++ costadelsoltapas/costadelsoltapas/Controllers/TapasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using costadelsoltapas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace costadelsoltapas.Controllers
+{
+    public class TapasController : Controller
+    {
+        private readonly ITapasRepository _tapasRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public TapasController(ITapasRepository tapasRepository, ICategoryRepository categoryRepository)
+        {
+            _tapasRepository = tapasRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_tapasRepository.AllTapas);
+        }
+    }
+}
---
+++ costadelsoltapas/costadelsoltapas/Models/ITapasRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace costadelsoltapas.Models
+{
+    public interface ITapasRepository
+    {
+        IEnumerable<Tapas> AllTapas { get; }
+        IEnumerable<Tapas> TapasOfTheWeek { get; }
+        Tapas GetTapasById(int TapasId);
+    }
+}
